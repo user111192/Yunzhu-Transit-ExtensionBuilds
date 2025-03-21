7ffa0e1ea61101451ab566649d71a4a552b36a70
X-Funny <79325059+X-Funny@users.noreply.github.com>
2025-03-21 21:06:31 +0800

    修复一些bug，上传miconic10zline3缩略图

--- src/main/java/top/xfunny/block/SchindlerZLine3Keypad1.java
+++ src/main/java/top/xfunny/block/SchindlerZLine3Keypad1.java
@@ -121,6 +121,7 @@ public class SchindlerZLine3Keypad1 extends LiftDestinationDispatchTerminalBase 
                     data1.switchScreen("schindler_z_line_3_keypad_1_key_mapping_accessibility");
                 } else if ("lobby".equals(output)) {
                     String callResult = data.callLift(world, pos, "1");
+                    callResult = callResult.equals("?") ? data.callLift(world, pos, "G") : callResult;
                     handleLiftCall.accept("schindler_z_line_3_keypad_1_key_mapping_home", callResult);
                 }
             }
@@ -133,6 +134,7 @@ public class SchindlerZLine3Keypad1 extends LiftDestinationDispatchTerminalBase 
                     data1.addInputString(world, pos, "1", true, data1, data);
                 } else if ("lobby".equals(output)) {
                     String callResult = data.callLift(world, pos, "1");
+                    callResult = callResult.equals("?") ? data.callLift(world, pos, "G") : callResult;
                     handleLiftCall.accept("schindler_z_line_3_keypad_1_key_mapping_home", callResult);
                 }
             }
@@ -148,6 +150,7 @@ public class SchindlerZLine3Keypad1 extends LiftDestinationDispatchTerminalBase 
                     data1.switchScreen("schindler_z_line_3_keypad_1_key_mapping_accessibility");
                 } else if ("lobby".equals(output)) {
                     String callResult = data.callLift(world, pos, "1");
+                    callResult = callResult.equals("?") ? data.callLift(world, pos, "G") : callResult;
                     handleLiftCall.accept("schindler_z_line_3_keypad_1_key_mapping_home", callResult);
                 }
             }
