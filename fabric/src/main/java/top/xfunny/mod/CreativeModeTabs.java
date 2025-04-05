05cc7981baef06447961947c711131cc437e8075
MrNTKey <108271360+MrNTKey@users.noreply.github.com>
2025-04-05 17:52:34 +0800

    更新资源文件

--- fabric/src/main/java/top/xfunny/mod/CreativeModeTabs.java
+++ fabric/src/main/java/top/xfunny/mod/CreativeModeTabs.java
@@ -16,7 +16,7 @@ public class CreativeModeTabs {
     static {
         YTE_LIFT_FIXTURES = Init.REGISTRY.createCreativeModeTabHolder(new Identifier(Init.MOD_ID, "yte_lift_fixtures"), () -> new ItemStack(new ItemConvertible(Items.FLOOR_AUTO_SETTER.get().data)));
         YTE_LIFT_DOORS = Init.REGISTRY.createCreativeModeTabHolder(new Identifier(Init.MOD_ID, "yte_lift_doors"), () -> new ItemStack(new ItemConvertible(Blocks.SCHINDLER_QKS9_DOOR_1.get().data)));
-        YTE_RAILWAY_FACILITIES = Init.REGISTRY.createCreativeModeTabHolder(new Identifier(Init.MOD_ID, "yte_railway_facilities"), () -> new ItemStack(new ItemConvertible(Items.SCHINDLER_QKS9_DOOR_1.get().data)));
+        YTE_RAILWAY_FACILITIES = Init.REGISTRY.createCreativeModeTabHolder(new Identifier(Init.MOD_ID, "yte_railway_facilities"), () -> new ItemStack(new ItemConvertible(Blocks.PAT_P01_TICKET_BARRIER_ENTRANCE.get().data)));
         YTE_TOOLS = Init.REGISTRY.createCreativeModeTabHolder(new Identifier(Init.MOD_ID, "yte_tools"), () -> new ItemStack(new ItemConvertible(Items.YTE_GROUP_LIFT_BUTTONS_LINK_CONNECTOR.get().data)));
     }
 
