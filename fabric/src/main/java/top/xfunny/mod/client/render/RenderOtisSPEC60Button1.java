c1207dc87f29af71d745837d43e0ab525ca0cfb3
QBYQ <q15118029270@163.com>
2025-06-24 01:45:21 -0500

    Fixed OTIS SPEC 60 ImageView.

--- fabric/src/main/java/top/xfunny/mod/client/render/RenderOtisSPEC60Button1.java
+++ fabric/src/main/java/top/xfunny/mod/client/render/RenderOtisSPEC60Button1.java
@@ -89,7 +89,7 @@ public class RenderOtisSPEC60Button1 extends BlockEntityRenderer<OtisSPEC60Butto
         buttonLayout.setBasicsAttributes(world, blockPos);
         buttonLayout.setWidth(LayoutSize.MATCH_PARENT);
         buttonLayout.setHeight(LayoutSize.MATCH_PARENT);
-        buttonLayout.setMargin(0, 0.4F / 16, 0, 0);
+        buttonLayout.setMargin(0, 0.05F / 16, 0, 0);
 
         final LinearLayout buttonContainer = new LinearLayout(true);
         buttonContainer.setBasicsAttributes(world, blockPos);
@@ -200,7 +200,7 @@ public class RenderOtisSPEC60Button1 extends BlockEntityRenderer<OtisSPEC60Butto
                 liftFloorDisplayView.setWidth(1.5F / 16);
                 liftFloorDisplayView.setLetterSpacing(10);
                 liftFloorDisplayView.setHeight(1.7F / 16);
-                liftFloorDisplayView.setMargin(-0.225F / 16, 2.5F / 16, 0.3F / 16, 0);
+                liftFloorDisplayView.setMargin(-0.225F / 16, -0.3F / 16, 0.3F / 16, 0);
 
                 liftFloorDisplayView.setTextAlign(TextView.HorizontalTextAlign.RIGHT);
 
@@ -210,7 +210,7 @@ public class RenderOtisSPEC60Button1 extends BlockEntityRenderer<OtisSPEC60Butto
                 liftArrowViewUP.setTexture(ARROW_TEXTURE);
                 liftArrowViewUP.setQueuedRenderLayer(QueuedRenderLayer.LIGHT_TRANSLUCENT);
                 liftArrowViewUP.setDimension(0.5F / 16);
-                liftArrowViewUP.setMargin(0, 1.07F / 16, 0, 0);
+                liftArrowViewUP.setMargin(0, 4.07F / 16, 0, 0);
                 liftArrowViewUP.setGravity(Gravity.CENTER_HORIZONTAL);
                 liftArrowViewUP.setColor(0xFFFF0000);
 
@@ -218,8 +218,8 @@ public class RenderOtisSPEC60Button1 extends BlockEntityRenderer<OtisSPEC60Butto
                 liftArrowViewDown.setBasicsAttributes(world, blockPos, sortedPositionsAndLifts.get(i).right(), LiftArrowView.ArrowType.DOWN);
                 liftArrowViewDown.setTexture(ARROW_TEXTURE);
                 liftArrowViewDown.setQueuedRenderLayer(QueuedRenderLayer.LIGHT_TRANSLUCENT);
-                liftArrowViewDown.setDimension(0.7F / 16);
-                liftArrowViewDown.setMargin(0, 1.07F / 16, 0, 0);
+                liftArrowViewDown.setDimension(0.5F / 16);
+                liftArrowViewDown.setMargin(0, 4.07F / 16, 0, 0);
                 liftArrowViewDown.setGravity(Gravity.CENTER_HORIZONTAL);
                 liftArrowViewDown.setColor(0xFFFF0000);
 
